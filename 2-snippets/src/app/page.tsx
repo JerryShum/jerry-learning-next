af426b34a3306151184ee961795e21575f5d8976
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2024-12-16 00:18:53 -0500

    snippets pages

--- 2-snippets/src/app/page.tsx
+++ 2-snippets/src/app/page.tsx
@@ -1,15 +1,30 @@
 import { db } from '@/db';
+import Link from 'next/link';
 
 export default async function Home() {
    const snippets = await db.snippet.findMany();
    const renderedSnippets = snippets.map((snippet) => {
-      return <div key={snippet.id}>{snippet.title}</div>;
+      return (
+         <Link
+            className="flex justify-between items-center p-2 border rounded"
+            href={`/snippets/${snippet.id}`}
+            key={snippet.id}
+         >
+            <div>{snippet.title}</div>
+            <div>View</div>
+         </Link>
+      );
    });
 
    return (
       <div>
-         <h1>home page</h1>
-         <div>{renderedSnippets}</div>
+         <div className="flex m-2 justify-between items-center">
+            <h1 className="text-xl font-bold">Snippets</h1>
+            <Link href={'/snippet/new'} className=" p-2 border rounded">
+               New
+            </Link>
+         </div>
+         <div className="flex flex-col gap-2">{renderedSnippets}</div>
       </div>
    );
 }
--- 2-snippets/src/app/snippets/[snippetID]/page.tsx
+++ 2-snippets/src/app/snippets/[snippetID]/page.tsx
@@ -21,7 +21,16 @@ export default async function SnippetPage(props: SnippetShowPageProps) {
 
    return (
       <div>
-         {snippet.title} | {snippet.code}
+         <div className="flex m-4 justify-between items-center">
+            <h1 className="text-xl font-bold">{snippet.title}</h1>
+            <div className="flex gap-4">
+               <button className="p-2 border rounded">Edit</button>
+               <button className="p-2 border rounded">Delete</button>
+            </div>
+         </div>
+         <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+            <code>{snippet.code}</code>
+         </pre>
       </div>
    );
 }
