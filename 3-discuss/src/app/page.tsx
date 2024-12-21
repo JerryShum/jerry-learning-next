32fd3037389d2ff3c521801363ac30dc156333e0
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2024-12-21 13:11:41 -0500

    setting up session providers

--- 3-discuss/src/app/page.tsx
+++ 3-discuss/src/app/page.tsx
@@ -1,11 +1,30 @@
 // "use client"
 
 import { Button } from '@nextui-org/button';
+import * as actions from '@/actions';
+import { auth } from '@/auth';
+import Profile from '@/components/Profile';
 
-export default function Home() {
+export default async function Home() {
+   const session = await auth();
+   console.log(session);
+
    return (
       <div>
-         <Button>Click Me</Button>
+         <form action={actions.signIn}>
+            <Button type="submit">Click Me Sign IN</Button>
+         </form>
+         <form action={actions.signOut}>
+            <Button type="submit">Click Me Sign OUT</Button>
+         </form>
+
+         {session?.user ? (
+            <div>Signed In {JSON.stringify(session.user)}</div>
+         ) : (
+            <div>Not signed in</div>
+         )}
+
+         <Profile />
       </div>
    );
 }
