0e25d1b17825e108dbd70743d93851730aec26f2
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2024-12-22 17:26:55 -0500

    using zod to add form validation in server action

--- 3-discuss/src/actions/create-topic.ts
+++ 3-discuss/src/actions/create-topic.ts
@@ -1,10 +1,29 @@
 'use server';
 
-export async function createTopic(formData: FormData) {
-   const name = formData.get('name');
-   const description = formData.get('description');
+import { z } from 'zod';
 
-   console.log(name, description);
+const createTopicSchema = z.object({
+   name: z
+      .string()
+      .min(3)
+      .regex(/^[a-z-]+$/, {
+         message: 'Must be lowercase letters or dashes without spaces.',
+      }),
+   description: z.string().min(10),
+});
+
+export async function createTopic(formData: FormData) {
+   const result = createTopicSchema.safeParse({
+      name: formData.get('name'),
+      description: formData.get('description'),
+   });
+
+   console.log(result);
+
+   // If there was an error in the information we parsed (validation failed):
+   if (!result.success) {
+      console.log(result.error.flatten().fieldErrors);
+   }
 
    // TODO: Revalidate homepage after creating topic
 }
