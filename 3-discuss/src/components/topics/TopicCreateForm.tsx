92f6e4e00835aed27b1e3f2f8177203bc24b4dd2
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2024-12-22 14:32:00 -0500

    Creating a form using next-ui to create a topic

--- 3-discuss/src/components/topics/TopicCreateForm.tsx
+++ 3-discuss/src/components/topics/TopicCreateForm.tsx
@@ -1,5 +1,39 @@
 import React from 'react';
 
+import {
+   Input,
+   Button,
+   Textarea,
+   Popover,
+   PopoverTrigger,
+   PopoverContent,
+} from '@nextui-org/react';
+import * as actions from '@/actions';
+
 export default function TopicCreateForm() {
-   return <div>TopicCreateForm</div>;
+   return (
+      <Popover placement="left-start" backdrop="blur" showArrow={true}>
+         <PopoverTrigger>
+            <Button color="primary">Create a Topic</Button>
+         </PopoverTrigger>
+         <PopoverContent>
+            <form action={actions.createTopic}>
+               <div className="flex flex-col gap-4 p-4 w-80 justify-center">
+                  <h3 className="text-lg">Create a Topic</h3>
+                  <Input
+                     label="name"
+                     labelPlacement="outside"
+                     placeholder="Name"
+                  />
+                  <Textarea
+                     label="Description"
+                     labelPlacement="outside"
+                     placeholder="Describe your topic"
+                  />
+                  <Button type="submit"> Submit</Button>
+               </div>
+            </form>
+         </PopoverContent>
+      </Popover>
+   );
 }
